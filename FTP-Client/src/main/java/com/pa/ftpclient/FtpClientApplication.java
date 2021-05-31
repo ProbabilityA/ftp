907c8acd82f6c3e456ec68567f51887e0b717a67
Pa <probabilitya@outlook.com>
2021-05-31 15:31:04 +0800

    feat(): initial commit

---
+++ FTP-Client/src/main/java/com/pa/ftpclient/FtpClientApplication.java
@@ -0,0 +1,13 @@
+package com.pa.ftpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtpClientApplication.class, args);
+    }
+
+}
---
+++ FTP-Server/src/main/java/com/pa/ftpserver/FtpServerApplication.java
@@ -0,0 +1,13 @@
+package com.pa.ftpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtpServerApplication.class, args);
+    }
+
+}
