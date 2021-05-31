2a39a09449426f06c3e82d35c86d81c5a6dc9184
Pa <probabilitya@outlook.com>
2021-05-31 15:27:37 +0800

    feat(): initial commit

---
+++ src/main/java/com/pa/FtpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pa/FtpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
